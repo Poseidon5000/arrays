86402fdd286b1c76079a91cb4ac6cf6e71fff8bd
Poseidon5000 <106795961+Poseidon5000@users.noreply.github.com>
2022-07-12 11:37:20 +0200

    Add files via upload

---
+++ array/a.js
@@ -0,0 +1,17 @@
+ let numberSum = [68, -68, 27, 96, 72, -25, -51, 32, 10, 64, -94, 4, 34, -86, 90, 81, 20, -56, -91, -50];
+
+ let newValue = 0;
+ function sumArr(arr){
+ for(let i=0; i < arr.length; i++){
+    newValue= newValue + arr[i]
+    return newValue
+ }
+ }
+ console.log(sumArr(numberSum));
+
+
+
+ 
+
+
+
